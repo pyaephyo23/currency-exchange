de95009d5ec3b0dbac2ce2541ef76a285a3b4836
Pyae Phyo Kyaw <137527401+pyaephyo23@users.noreply.github.com>
2024-08-09 19:59:46 +0630

    Add project files.

---
+++ Tutorial_01/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Tutorial_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter currency name :");
+            string fromCurrencyName = Console.ReadLine();
+
+            double fromCurrencyAmount;
+            Console.Write("Enter currency amount :");
+
+            while (true)
+            {
+                
+                if (double.TryParse(Console.ReadLine(), out fromCurrencyAmount))
+                {
+                    fromCurrencyAmount = Math.Round(fromCurrencyAmount, 2);
+                    break;
+                }
+                else
+                {
+                    Console.Write("Please enter currency amount again :");
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Enter currency name :");
+            string toCurrencyName = Console.ReadLine();
+
+            double exchangeRate;
+            Console.Write("Enter exchange rate :");
+
+            while (true)
+            {
+
+                if (double.TryParse(Console.ReadLine(), out exchangeRate))
+                {
+           
+                    break;
+                }
+                else
+                {
+                    Console.Write("Please enter exchange rate again :");
+
+                }
+            }
+
+            double toCurrencyAmount = exchangeRate * fromCurrencyAmount;
+            toCurrencyAmount = Math.Round(toCurrencyAmount, 6);
+            Console.WriteLine();
+            Console.WriteLine($"Form Currency     {fromCurrencyAmount} {fromCurrencyName}");
+            Console.WriteLine($"Exchange Rate     1 {fromCurrencyName} = {exchangeRate} {toCurrencyName}");
+            Console.WriteLine($"To Currency       {toCurrencyAmount} {toCurrencyName}");
+        }
+    }
+}
